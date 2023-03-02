9ce016b69a218911ae0dca5b5acf1383f05b9240
Kars de Jong <jongk@linux-m68k.org>
2023-03-02 08:59:47 +0100

    Use async_forward_entry_setups (#89)
    Replace deprecated async_setup_platforms with async_forward_entry_setups

--- custom_components/luxtronik/__init__.py
+++ custom_components/luxtronik/__init__.py
@@ -59,7 +59,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
     luxtronik = hass.data[DOMAIN]
 
-    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     def logout_luxtronik(event: Event) -> None:
         """Close connections to this heatpump."""
