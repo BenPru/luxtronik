9e7f06c4d0881156e254113655082f2ab6d95c6b
BenPru <32298537+BenPru@users.noreply.github.com>
2024-12-31 13:00:15 +0100

    Aktualisieren von model.py
    #309

--- custom_components/luxtronik/model.py
+++ custom_components/luxtronik/model.py
@@ -173,8 +173,7 @@ class LuxtronikClimateDescription(
 
 def metaclass_resolver(*classes):
     metaclass = tuple(set(type(cls) for cls in classes))
-    metaclass = metaclass[0] if len(metaclass)==1 \
-                else type("_".join(mcls.__name__ for mcls in metaclass), metaclass, {})   # class M_C
+    metaclass = metaclass[0] if len(metaclass)==1 else type("_".join(mcls.__name__ for mcls in metaclass), metaclass, {})   # class M_C
     return metaclass("_".join(cls.__name__ for cls in classes), classes, {})   
 
 @dataclass
